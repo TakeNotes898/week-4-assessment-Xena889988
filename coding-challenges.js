ccd78caedf8a1d677f90a6f5ef616949db82e3b9
Ubuntu <ubuntu@ip-172-31-25-22.us-east-2.compute.internal>
2020-03-02 17:14:01 +0000

    Finish assessment

--- coding-challenges.js
+++ coding-challenges.js
@@ -1,60 +1,118 @@
 // ASSESSMENT 4: JavaScript Coding Practical Questions
 
-// --------------------1) Create a function that takes in an array. Each time the function is run, remove the first item from the array and shuffle the remaining content. If the array is empty, return "The array is empty."
+// --------------------
+// // 1) Create a function that takes in an array. 
+// // Each time the function is run, 
+// // remove the first item from the array and 
+// shuffle the remaining content. 
+// If the array is empty, return "The array is empty."
 
+var collections = ["purple", "blue", "green", "yellow", "pink"];
 
+function arrMixer(array) {
+   
+    for (let i = array.length - 1; i > 0; i--) {
+        const newI = Math.floor(Math.random() * (i + 1));
+        [array[i], array[newI]] = [array[newI], array[i]];
+    }
+    if (array.shift() >= 0){
+    }
+ return array;
+ // else {
+    // return 'empty array';
+    // }
+}
 
-
-
-var collections = ["purple", "blue", "green", "yellow", "pink"]
+console.log(arrMixer(collections));
+console.log(arrMixer(collections));   
+console.log(arrMixer(collections));   
+console.log(arrMixer(collections));  
+ 
 // Expected output example1 (can be a different order): ["yellow", "blue", "pink", "green"]
 // Expected output example2 (can be a different order): ["blue", "green", "pink"]
 // Expected output example3 (can be a different order): ["pink", "green"]
 
 
-// --------------------2) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
-
-
-
-
-
-var cubeAndSum1 = [2, 3, 4]
+// // --------------------
+// 2) Create a function that 
+// takes in an array of numbers and 
+// returns the sum of all the numbers cubed.
+var cubeAndSum1 = [2, 3, 4];
 // Expected output: 99
-var cubeAndSum2 = [0, 5, 10]
+var cubeAndSum2 = [0, 5, 10];
 // Expected output: 1125
 
+const arrCube = (cube) => {
+    let newCubeArray = 0;
+    for (let i=0; i< cube.length; i++) {
+        newCubeArray = (cube[0]**3+cube[1]**3+cube[2]**3)
+    }
+    return newCubeArray;
+};
+
+console.log(arrCube(cubeAndSum1));   
+console.log(arrCube(cubeAndSum2));
 
 
-// --------------------3) Create a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
-
-
-
-
-
-var nums1 = [3, 56, 90, -8, 0, 23, 6]
+// --------------------
+// 3) Create a function that 
+// takes an array of numbers and 
+// returns an array of the minimum and maximum numbers in that order.
+var nums1 = [3, 56, 90, -8, 0, 23, 6];
 // Expected output: [-8, 90]
-var nums2 = [109, 5, 9, -59, 8, 24]
+var nums2 = [109, 5, 9, -59, 8, 24];
 // Expected output: [-59, 109]
 
+function arrMinMax(nummm) {
+  return [Math.min(...nummm), Math.max(...nummm)];
+}
 
-// --------------------4) Create a function that takes in a string and returns a string with every other letter capitalized.
+console.log(arrMinMax(nums1));   
+console.log(arrMinMax(nums2));
 
 
+// --------------------
+// 4) Create a function that 
+// takes in a string and 
+// returns a string with every other letter capitalized.
 
 
-
-var testString1 = "albatross"
+var testString1 = "albatross";
 // Expected output: "aLbAtRoSs"
-var testString2 = "jabberwocky"
+var testString2 = "jabberwocky";
 // Expected output: "jAbBeRwOcKy"
 
+function strCapIt (str) {
+    let strSuper = '';
+    for ( i = 0; i < str.length; i++) {
+        strSuper += i % 2 == 1 ? str.charAt(i).toUpperCase() : str.charAt(i);
+  }
+  return strSuper;  
+}
 
-// --------------------5) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator.
+console.log(strCapIt(testString1));   
+console.log(strCapIt(testString2));
 
 
 
+// --------------------
+// 5) Create a function that 
+// takes in two arrays as arguments and 
+// returns one array with no duplicate values. 
+// STRETCH: Use the spread operator.
 
-
-var arr1 = [3, 7, 10, 5, 4, 3, 3]
-var arr2 = [7, 8, 2, 3, 1, 5, 4]
+var arr1 = [3, 7, 10, 5, 4, 3, 3];
+var arr2 = [7, 8, 2, 3, 1, 5, 4];
 // Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
+var arr3 = [...arr1, ...arr2];
+
+// arr3.sort();
+
+function duplicateArray (removeDouble)  {
+    var a = [];
+    for (var i=0, l=removeDouble.length; i<l; i++)
+        if (a.indexOf(removeDouble[i]) === -1)
+            a.push(removeDouble[i]);
+    return a;
+}
+console.log(duplicateArray(arr3));
--- interview-questions.md
+++ interview-questions.md
@@ -4,38 +4,38 @@ Answer the following questions. First, without external resources. Challenge you
 
 1. What is an instance variable in Ruby? How is it different from other variables?
 
-  Your answer:
+  Your answer: belongs to a specific class (local var)
 
-  Researched answer:
+  Researched answer: In the Ruby programming language, an instance variable is a type of variable which starts with an @ symbol. An instance variable is used as part of Object-Oriented Programming (OOP) to give objects their own private space to store data. We say that objects can: Do things.
 
 
 
 2. What is a block in Ruby?
 
-  Your answer:
+  Your answer: a set of something like an object or a class
 
-  Researched answer:
+  Researched answer: A Ruby block is a way of grouping statements, and may appear only in the source adjacent to a method call; the block is written starting on the same line as the method call's last parameter (or the closing parenthesis of the parameter list). The code in the block is not executed at the time it is encountered.
 
 
 
 3. Ruby has an implicit return. What does that mean?
 
-  Your answer:
+  Your answer: i would guess that it is a command that functions inside the code but is not printed
 
-  Researched answer:
+  Researched answer: Any statement in Ruby returns the value of the last evaluated expression. A simple example is the getter method. We call a method and expect some value in return. But as we know, Ruby always returns the last evaluated expression
 
 
 
 4. What is object-oriented programming? How is it different than functional programming?
 
-  Your answer:
+  Your answer: as in ruby it is straight forward. Understandable why a command goes in a specific spot.
 
-  Researched answer:
+  Researched answer: Object-oriented programming (OOP) refers to a type of computer programming (software design) in which programmers define the data type of a data structure, and also the types of operations (functions) that can be applied to the data structure.
 
 
 
 5. What is the difference between a class and an object?
 
-  Your answer:
+  Your answer: class is a parent component that is responsible for all the objects
 
-  Researched answer:
+  Researched answer: Class: Ruby separates everything into classes. Like integers, floats and strings. Method: These are the things that you can do with an object. For example, you can add integers together, so + is a method.
